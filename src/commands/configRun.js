d002464004211060371fdb187808d5d16def2755
Aleksandr Selivanov <work.a.selivanov@gmail.com>
2025-12-16 11:41:32 +0300

    feat: progress in function Run configurations
    - Implemented a mechanism for selecting the target architecture for the **Run configurations** function.
    - The architecture selected by the user is saved in the extension settings file.
    - Implemented a mechanism for selecting programming tools for ARM microcontrollers for the **Run configurations** function.
    - The tool selected by the user is saved in the extension settings file.
    - "selectedPlatform", "selectedUSB" and "selectedTool" fields have been added to package.json.
    
    fix: CHANGELOG.md
    
    - In the file CHANGELOG.md fixed an error in naming version 0.1.5.

--- src/commands/configRun.js
+++ src/commands/configRun.js
@@ -3,6 +3,61 @@ const vscode = require("vscode");
 async function configRun() {
     vscode.window.showInformationMessage("Configure run...");
 
+    const selectedPlatform = await vscode.window.showQuickPick(
+        [
+            { label: 'ARM', description: 'Select ARM architecture' },
+            { label: 'ESP', description: 'Select ESP architecture' }
+        ],
+        {
+            placeHolder: 'Select target platform for new NuttX project',
+            title: 'NuttX Project Platform'
+        }
+    );
+
+// Проверка, что пользователь сделал выбор, либо его отменил
+if (!selectedPlatform) {
+    vscode.window.showWarningMessage('The architecture was not chosen');
+    return; 
+}
+
+// Сохраняем то, что выбрали
+const platform = selectedPlatform.label; // Либо STM32, либо ESP32
+
+// Теперь нужно сохранить настрйоки, как в configPaths и прочих функциях:
+const config = vscode.workspace.getConfiguration('nuttx');
+await config.update('selectedPlatform', platform, vscode.ConfigurationTarget.Workspace);
+
+// Теперь смотрим, что выбрал пользователь выбрал
+if(platform === 'ARM') {
+    const selectedTools = await vscode.window.showQuickPick(
+    [
+        { label: 'stm32flash', description: 'Programming the chip via a standard UART bootloader' },
+        { label: 'st-flash', description: 'Programming through a prorgammator ST-Link' },
+        { label: 'OpenOCD', description: 'An open debugging and firmware tool via JTAG/SWD' },
+    ],
+    {
+        placeHolder: 'Choose a chip programming tool',
+        title: 'NuttX Programming Tool'
+    }
+    );
+
+    if(!selectedTools) {
+        vscode.window.showWarningMessage('Programming tools was not chosen');
+        return;
+    }
+
+    // Сохраняем инструмент для работы
+    const tool = selectedTools.label;
+    const toolConfig = vscode.workspace.getConfiguration('nuttx');
+    await toolConfig.update('selectedTool', tool, vscode.ConfigurationTarget.Workspace);
+
+}
+else if(platform === 'ESP') {
+    vscode.window.showInformationMessage("Choose ESP");
+}
+
+
+        /*
     try {
         const usb_devices = await navigator.usb.getDevices();
         console.log('Подключенные USB-устройства:', usb_devices);
@@ -11,6 +66,7 @@ async function configRun() {
     } catch (error) {
         console.error('Ошибка при получении USB-устройств:', error);
     }
+        */
 }
 
 module.exports = { configRun };
--- src/commands/printInfo.js
+++ src/commands/printInfo.js
@@ -1,7 +1,7 @@
 const vscode = require("vscode");
 
 function printInfo() {
-  vscode.window.showErrorMessage('VSCode Web extension for NuttX.', { modal: true, detail: 'Version: 0.1.5' });
+  vscode.window.showErrorMessage('VSCode Web extension for NuttX.', { modal: true, detail: 'Version: 0.1.6' });
 }
 
 module.exports = { printInfo };
